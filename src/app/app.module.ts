0597deed59df51d08627546843be18706f967a7a
du2python <du.pak.py2@gmail.com>
2021-11-20 15:39:11 +0900

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+//import { ThirdModule } from './third/third.module';
+import { RouterModule,Routes} from '@angular/router'; //라우터
+const router : Routes = [  //라우팅
+  {path : '' , redirectTo:'first', pathMatch:'full'},
+  {path : 'first' , component : FirstComponent},  
+  {path : 'second' , loadChildren: () => import('./third/third.module').then(m => m.ThirdModule) },
+  {path : '**' , redirectTo:'first', pathMatch:'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(router,{enableTracing:false}),
   ],
   providers: [],
   bootstrap: [AppComponent]
